6c231e5f70ddb394623563a5e672aa4095dbfac1
taanasaa <said.naghiyev@swisscom.com>
2023-08-21 20:37:59 +0300

    importing text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MenubarModule} from "primeng/menubar";
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MenubarModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    InputTextModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
