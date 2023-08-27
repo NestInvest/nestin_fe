5db6c190c703b66304b202e1fe4f9acf277d2f91
Azimov Makhmudjon <Makhmudjon.Azimov@swisscom.com>
2023-08-28 02:03:19 +0300

    NES-8-Fetching lists - DTO updated accordingly

--- src/lib/model/property-detail.model.ts
+++ src/lib/model/property-detail.model.ts
@@ -1,6 +1,13 @@
-export interface PropertyDetailModel{
-  id: number;
+export interface PropertyDetailModel {
+  propertyId: number;
   propertyName: string;
-  address: string;
+  propertyType: string;
   price: number;
+  bedrooms: number;
+  bathrooms: number;
+  location: string[];
+  area: string;
+  description: string;
+  features: string[];
+  contact: string[];
 }
